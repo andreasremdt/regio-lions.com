f8e78048c9ba8b2a57d3fcf318e2b3a9e797205f
Andreas Remdt <me@andreasremdt.com>
2025-10-26 09:49:39 +0100

    feat: initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,40 @@
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/users'
+import { Media } from './collections/media'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+    connectOptions: {
+      dbName: process.env.DATABASE_NAME || '',
+      appName: process.env.DATABASE_APP_NAME || '',
+      retryWrites: true,
+      writeConcern: {
+        w: 'majority',
+      },
+    },
+  }),
+  sharp,
+  plugins: [],
+})
